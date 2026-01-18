469991aaedfecca57c9ac15d960d8dd60bf50428
Hanif Mohammednasir <124556596+nifomohad@users.noreply.github.com>
2026-01-18 21:44:08 +0300

    fix fct table

--- medical_warehouse/models/marts/fct_messages.sql
+++ medical_warehouse/models/marts/fct_messages.sql
@@ -5,7 +5,13 @@ WITH stg_msgs AS (
 ),
 
 dim_channels AS (
+    -- Reference dim_channels to create DAG dependency
     SELECT channel_key, channel_name FROM {{ ref('dim_channels') }}
+),
+
+dim_dates AS (
+    -- Reference dim_dates to create the missing link in your DAG
+    SELECT date_key, full_date FROM {{ ref('dim_dates') }}
 )
 
 SELECT
@@ -14,7 +20,7 @@ SELECT
     
     -- 2. Foreign Keys (Connecting to Dimensions)
     c.channel_key, 
-    TO_CHAR(m.message_at, 'YYYYMMDD')::INT AS date_key,
+    d.date_key,
     
     -- 3. Descriptive Attributes
     m.message_content AS message_text,
@@ -28,6 +34,9 @@ SELECT
     m.is_media_attached AS has_image
 
 FROM stg_msgs m
--- Join to get the Surrogate Key from dim_channels
+-- Join with Channels to get the Surrogate Key
 INNER JOIN dim_channels c 
     ON m.channel_username = c.channel_name
+-- Join with Dates using the calculated date key to enforce the DAG relationship
+INNER JOIN dim_dates d 
+    ON TO_CHAR(m.message_at, 'YYYYMMDD')::INT = d.date_key
